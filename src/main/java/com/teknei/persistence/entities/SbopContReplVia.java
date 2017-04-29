5f665dcfa7cd8e6616f0b889ef25c3bdf8af8c61
amaro.coria@gmail.com <adeb2803>
2017-04-29 02:17:06 -0500

    Sincronization with QA

--- src/main/java/com/teknei/persistence/entities/SbopContReplVia.java
+++ src/main/java/com/teknei/persistence/entities/SbopContReplVia.java
@@ -16,61 +16,68 @@ import lombok.Data;
 
 /**
  * The persistent class for the sbop_cont_repl_via database table.
- * 
  */
 @Entity
 @Table(name = "sbop_cont_repl_via", schema = "sitm")
 @NamedQuery(name = "SbopContReplVia.findAll", query = "SELECT s FROM SbopContReplVia s")
 @Data
 public class SbopContReplVia implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@SequenceGenerator(name = "SBOP_CONT_REPL_VIA_ID_GENERATOR", sequenceName = "sitm.sbop_cont_repl_via_id_cont_repl_seq", allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SBOP_CONT_REPL_VIA_ID_GENERATOR")
-	private Long id;
+    @Id
+    @SequenceGenerator(name = "SBOP_CONT_REPL_VIA_ID_GENERATOR", sequenceName = "sitm.sbop_cont_repl_via_id_cont_repl_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SBOP_CONT_REPL_VIA_ID_GENERATOR")
+    private Long id;
 
-	@Column(name = "fch_crea")
-	private Timestamp fchCrea;
+    @Column(name = "fch_crea")
+    private Timestamp fchCrea;
 
-	@Column(name = "fch_modi")
-	private Timestamp fchModi;
+    @Column(name = "fch_modi")
+    private Timestamp fchModi;
 
-	@Column(name = "fch_oper")
-	private Timestamp fchOper;
+    @Column(name = "fch_oper")
+    private Timestamp fchOper;
 
-	@Column(name = "id_cont_repl")
-	private Integer idContRepl;
+    @Column(name = "id_cont_repl")
+    private Integer idContRepl;
 
-	@Column(name = "id_equi")
-	private Integer idEqui;
+    @Column(name = "id_equi")
+    private Integer idEqui;
 
-	@Column(name = "id_esta")
-	private Integer idEsta;
+    @Column(name = "id_esta")
+    private Integer idEsta;
 
-	@Column(name = "id_tipo")
-	private Integer idTipo;
+    @Column(name = "id_tipo")
+    private Integer idTipo;
 
-	@Column(name = "id_tipo_repl")
-	private Integer idTipoRepl;
+    @Column(name = "id_tipo_repl")
+    private Integer idTipoRepl;
 
-	@Column(name = "id_vehi")
-	private Integer idVehi;
+    @Column(name = "id_vehi")
+    private Integer idVehi;
 
-	@Column(name = "reg_disp_tot")
-	private Integer regDispTot;
+    @Column(name = "reg_disp_tot")
+    private Integer regDispTot;
 
-	@Column(name = "reg_envi_tot")
-	private Integer regEnviTot;
+    @Column(name = "reg_envi_tot")
+    private Integer regEnviTot;
 
-	@Column(name = "reg_pend_envi")
-	private Integer regPendEnvi;
+    @Column(name = "reg_pend_envi")
+    private Integer regPendEnvi;
 
-	@Column(name = "usr_crea")
-	private String usrCrea;
+    @Column(name = "usr_crea")
+    private String usrCrea;
 
-	@Column(name = "usr_modi")
-	private String usrModi;
+    @Column(name = "usr_modi")
+    private String usrModi;
 
+    @Column(name = "actu_pend_envi")
+    private Integer actuPendEnvi;
+
+    @Column(name = "deta_tipo_repl")
+    private String detaTipoRepl;
+
+    @Column(name = "obs_cont_repl")
+    private String obsContRepl;
 
 }
